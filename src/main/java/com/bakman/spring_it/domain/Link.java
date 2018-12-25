98670a097e3ca1f667aec076b4318bcabbcca851
igorbakman@gmail.com <igorbakman@gmail.com>
2018-12-25 11:30:42 +0100

    add mapping link2comment

--- src/main/java/com/bakman/spring_it/domain/Link.java
+++ src/main/java/com/bakman/spring_it/domain/Link.java
@@ -7,6 +7,9 @@ import lombok.NonNull;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
@@ -18,5 +21,8 @@ public class Link {
     private Long id;
     private @NonNull String title;
     private @NonNull String url;
+
+    @OneToMany(mappedBy = "link")
+    private List<Comment> comments = new ArrayList<>();
 
 }
