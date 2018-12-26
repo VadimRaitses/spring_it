546fbb17ad431d99619351b630151c4efef2fbb7
igorbakman@gmail.com <igorbakman@gmail.com>
2018-12-26 11:06:20 +0100

    add db config in the properties file and an example for properties load class

--- src/main/java/com/bakman/spring_it/SpringItApplication.java
+++ src/main/java/com/bakman/spring_it/SpringItApplication.java
@@ -1,14 +1,33 @@
 package com.bakman.spring_it;
 
+import com.bakman.spring_it.config.SprintItProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
+@EnableConfigurationProperties(SprintItProperties.class)
 public class SpringItApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringItApplication.class);
+
+    @Autowired
+    private SprintItProperties sprintItProperties;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringItApplication.class, args);
     }
 
+    @Bean
+    @Profile("dev")
+    CommandLineRunner runner() {
+        return args -> System.out.println("Welcome MSG: " + sprintItProperties.getWelcome());
+    }
 }
 
--- src/main/java/com/bakman/spring_it/config/SprintItProperties.java
+++ src/main/java/com/bakman/spring_it/config/SprintItProperties.java
@@ -1,4 +1,20 @@
 package com.bakman.spring_it.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("springit")
 public class SprintItProperties {
+
+    /**
+     * this is our custom properties file
+     */
+    private String welcome = "Hello";
+
+    public String getWelcome() {
+        return welcome;
+    }
+
+    public void setWelcome(String welcome) {
+        this.welcome = welcome;
+    }
 }
